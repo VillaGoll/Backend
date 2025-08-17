01d422e7b7fc99461843bac085dae7270476e4a4
VillaGoll <villagolvn@gmail.com>
2025-08-16 20:29:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -35,5 +35,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Server running on http://${HOST}:${PORT}`));
